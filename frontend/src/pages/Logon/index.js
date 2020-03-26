6f60572652dad1f9b9fa1b43e3eeb152c8cbad60
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2020-03-26 07:59:41 -0300

    Pagina de cadastro.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import {FiLogIn} from 'react-icons/fi';
 
 
-import './styles.css'
+import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logonImg from '../../assets/logo.svg';
@@ -15,14 +16,14 @@ export default function Logon() {
                 <img src={logonImg} alt="Be The Hero" />
 
                 <form>
-                    <h1>Façca seu Logon</h1>
+                    <h1>Faça seu Logon</h1>
 
                     <input placeholder="Sua ID" />
                     <button className="button">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Não Tenho Cadastro.
-                   </a>
+                   </Link>
                 </form>
             </section>
 
